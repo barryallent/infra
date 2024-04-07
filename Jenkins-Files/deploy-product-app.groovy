784a08a6c605866f2bdf7d78610ae1b03299db76
barryallent <abhinav31796@gmail.com>
2024-04-07 12:02:32 +0530

    install kubectl etc in jenkins and jenkins service role changed to clusteradmin (#5)
    Co-authored-by: Abhinav Tyagi <abhinav.t@zoom.us>

--- Jenkins-Files/deploy-product-app.groovy
+++ Jenkins-Files/deploy-product-app.groovy
@@ -5,8 +5,9 @@ pipeline {
         }
     }
     environment {
-        WORKING_DIRECTORY = '/home/'
+        WORKING_DIRECTORY = '/home'
         GIT_CREDENTIALS_ID = 'github-creds'
+        DOCKER_PASSWORD = credentials('docker-password')
         ENV = "dev"
     }
 
@@ -14,7 +15,7 @@ pipeline {
         stage('GIT CHECKOUT product CODE') {
             steps {
                 sh '''
-            mkdir -p ${WORKING_DIRECTORY}/product-app
+                    mkdir -p ${WORKING_DIRECTORY}/product-app
             '''
                 dir("${WORKING_DIRECTORY}/product-app") {
                     git branch: "${PRODUCT_APP_BRANCH}",
@@ -26,7 +27,7 @@ pipeline {
         stage('GIT CHECKOUT INFRA CODE') {
             steps {
                 sh '''
-            mkdir -p ${WORKING_DIRECTORY}/infra
+                    mkdir -p ${WORKING_DIRECTORY}/infra
             '''
                 dir("${WORKING_DIRECTORY}/infra") {
                     git branch: "${INFRA_BRANCH}",
@@ -35,16 +36,27 @@ pipeline {
                 }
             }
         }
-
-        stage('build docker image if need') {
+        stage('build docker image') {
             steps {
                 sh '''
-            echo ${DOCKER_PASSWORD} | docker login --username barryallenat --password-stdin
-            docker build --build-arg ENV=${ENV} -t barryallenat/spring-product-app:${ENV}-${BUILD_NUMBER} . --network host
-            docker push barryallenat/spring-product-app:${ENV}-${BUILD_NUMBER}
+                    cd ${WORKING_DIRECTORY}/product-app
+                    echo $DOCKER_PASSWORD | docker login --username barryallenat --password-stdin
+                    docker build --build-arg ENV=${ENV} -t barryallenat/spring-product-app:${ENV}-${BUILD_NUMBER} . --network host
+                    docker push barryallenat/spring-product-app:${ENV}-${BUILD_NUMBER}
+            '''
+            }
+        }
+        stage('Deploy product app') {
+            steps {
+                sh '''
+                    cd ${WORKING_DIRECTORY}/infra
+                    kubectl delete deploy product-app
+                    kubectl apply -f ./product-configmap.yml
+                    kubectl apply -f ./product-deployment.yml
+                    kubectl apply -f ./product-service.yml
+                    kubectl apply -f ./product-ingress.yml
             '''
             }
         }
     }
-
 }
